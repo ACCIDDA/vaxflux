2653038212ab3fd7fd20a060050a484c3eb06862
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-01-09 16:31:17 -0500

    Add `strata_change_detection` function
    Added a function to `vaxflux.model` to compare two model traces to
    measure the change in strata related parameters.

--- src/vaxflux/model.py
+++ src/vaxflux/model.py
@@ -1,8 +1,10 @@
-__all__ = ("build_model",)
+__all__ = ("build_model", "strata_change_detection")
 
 
-from typing import Any, Literal
+from collections.abc import Iterable
+from typing import Any, Callable, Literal
 
+from arviz import InferenceData
 import numpy as np
 import pandas as pd
 import pymc as pm
@@ -126,3 +128,63 @@ def build_model(
         )
 
     return model
+
+
+def strata_change_detection(
+    trace_before: InferenceData,
+    trace_after: InferenceData,
+    test_and_metrics: Iterable[dict[str, Callable]],
+    param: str | Iterable[str],
+) -> pd.DataFrame:
+    """
+    Compare two traces for given strata parameters.
+
+    Args:
+        trace_before: The trace before the change.
+        trace_after: The trace after the change.
+        test_and_metrics: A list of dictionaries with the test and metric to apply. The
+            first key is the name of the test and the second key is the name of the
+            metric.
+        param: The parameter(s) to compare.
+
+    Returns:
+        A pandas DataFrame with the columns "param", "name", "test", "metric",
+        "test_statistic", "p_value", and "metric_value".
+    """
+    test_and_metrics = (
+        [test_and_metrics] if isinstance(test_and_metrics, dict) else test_and_metrics
+    )
+    param = [param] if isinstance(param, str) else param
+
+    if not np.all(
+        trace_before.posterior.coords.get("strata").to_numpy()
+        == trace_after.posterior.coords.get("strata").to_numpy()
+    ):
+        raise ValueError(
+            "The 'strata' coordinates do not match between the two traces."
+        )
+
+    results = []
+    for p in param:
+        for s in trace_before.posterior.coords.get("strata").to_numpy().tolist():
+            x = trace_before.posterior[f"{p}Strata"].sel(strata=s).to_numpy().flatten()
+            y = trace_after.posterior[f"{p}Strata"].sel(strata=s).to_numpy().flatten()
+            for test_and_metric in test_and_metrics:
+                test_name, metric_name = test_and_metric.keys()
+                test_func, metric_func = test_and_metric.values()
+                test_result = test_func(x, y)
+                metric_value = metric_func(x, y)
+                results.append(
+                    {
+                        "param": p,
+                        "name": s,
+                        "test": test_name,
+                        "metric": metric_name,
+                        "test_statistic": test_result.statistic,
+                        "p_value": test_result.pvalue,
+                        "metric_value": metric_value,
+                    }
+                )
+    results = pd.DataFrame.from_records(results)
+
+    return results
