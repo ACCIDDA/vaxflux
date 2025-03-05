971d933adfd94fb41cbab3270f669ee842369111
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-03-05 16:30:05 -0500

    Consolidate `_pm_name` and `_coord_name`

--- src/vaxflux/_util.py
+++ src/vaxflux/_util.py
@@ -2,20 +2,30 @@ __all__: tuple[str, ...] = ()
 
 
 import re
+from typing import Callable
+
+
+_CLEAN_TEXT_REGEX = re.compile(r"[^a-zA-Z0-9]")
 
 
 def _clean_text(text: str) -> str:
     return re.sub(r"[^a-zA-Z0-9]", " ", text).title().replace(" ", "")
 
 
-def _pm_name(*args: str | None) -> str:
-    return "".join(map(_clean_text, filter(None, args)))
-
-
-def _coord_name(*args: str | None) -> str:
-    return "_".join(
+def _clean_name(
+    *args: str | None, joiner: str = "", transform: Callable[[str], str] = lambda x: x
+) -> str:
+    return joiner.join(
         map(
-            lambda x: re.sub(r"[^a-zA-Z0-9]", " ", x).lower().replace(" ", "_"),
+            lambda x: transform(_CLEAN_TEXT_REGEX.sub(" ", x)).replace(" ", joiner),
             filter(None, args),
         )
     )
+
+
+def _pm_name(*args: str | None) -> str:
+    return _clean_name(*args, transform=lambda x: x.title())
+
+
+def _coord_name(*args: str | None) -> str:
+    return _clean_name(*args, joiner="_", transform=lambda x: x.lower())
