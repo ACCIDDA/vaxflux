93de04d664b1926a86261ff076eb0d258554865c
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-05-14 14:18:45 -0400

    Unit test for exact results

--- tests/dates/test__infer_ranges_from_observations.py
+++ tests/dates/test__infer_ranges_from_observations.py
@@ -354,3 +354,257 @@ def test_ranges_is_subset_of_output_when_observations_provided(
         if observations is None
         else set(ranges).issubset(set(output))
     )
+
+
+@pytest.mark.parametrize(
+    ("observations", "ranges", "mode", "expected"),
+    [
+        # No observations, only ranges
+        (
+            None,
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2023-12-01",
+                    end_date="2023-12-31",
+                    report_date="2024-01-01",
+                ),
+            ],
+            "date",
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2023-12-01",
+                    end_date="2023-12-31",
+                    report_date="2024-01-01",
+                ),
+            ],
+        ),
+        # No ranges, only observations
+        (
+            pd.DataFrame(
+                data={
+                    "season": ["2023/24"],
+                    "start_date": ["2023-12-01"],
+                    "end_date": ["2023-12-31"],
+                    "report_date": ["2024-01-01"],
+                }
+            ),
+            [],
+            "date",
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2023-12-01",
+                    end_date="2023-12-31",
+                    report_date="2024-01-01",
+                ),
+            ],
+        ),
+        # Non-overlapping observations and ranges
+        (
+            pd.DataFrame(
+                data={
+                    "season": ["2023/24"],
+                    "start_date": ["2023-12-01"],
+                    "end_date": ["2023-12-31"],
+                    "report_date": ["2024-01-01"],
+                }
+            ),
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2024-01-01",
+                    end_date="2024-01-31",
+                    report_date="2024-02-01",
+                ),
+            ],
+            "date",
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2023-12-01",
+                    end_date="2023-12-31",
+                    report_date="2024-01-01",
+                ),
+                DateRange(
+                    season="2023/24",
+                    start_date="2024-01-01",
+                    end_date="2024-01-31",
+                    report_date="2024-02-01",
+                ),
+            ],
+        ),
+        # Ranges contained within observations
+        (
+            pd.DataFrame(
+                data={
+                    "season": ["2023/24", "2023/24"],
+                    "start_date": ["2023-12-01", "2024-01-01"],
+                    "end_date": ["2023-12-31", "2024-01-31"],
+                    "report_date": ["2024-01-01", "2024-02-01"],
+                }
+            ),
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2024-01-01",
+                    end_date="2024-01-31",
+                    report_date="2024-02-01",
+                ),
+            ],
+            "date",
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2023-12-01",
+                    end_date="2023-12-31",
+                    report_date="2024-01-01",
+                ),
+                DateRange(
+                    season="2023/24",
+                    start_date="2024-01-01",
+                    end_date="2024-01-31",
+                    report_date="2024-02-01",
+                ),
+            ],
+        ),
+        # Duplicated ranges contained within observations
+        (
+            pd.DataFrame(
+                data={
+                    "season": 10 * ["2023/24"],
+                    "start_date": 10 * ["2023-12-01"],
+                    "end_date": 10 * ["2023-12-31"],
+                    "report_date": 10 * ["2024-01-01"],
+                    "value": list(range(10)),
+                }
+            ),
+            [],
+            "date",
+            [
+                DateRange(
+                    season="2023/24",
+                    start_date="2023-12-01",
+                    end_date="2023-12-31",
+                    report_date="2024-01-01",
+                ),
+            ],
+        ),
+        # No observations, only ranges
+        (
+            None,
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+                SeasonRange(
+                    season="2024/25", start_date="2024-11-01", end_date="2025-01-31"
+                ),
+            ],
+            "season",
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+                SeasonRange(
+                    season="2024/25", start_date="2024-11-01", end_date="2025-01-31"
+                ),
+            ],
+        ),
+        # No ranges, only observations
+        (
+            pd.DataFrame(
+                data={
+                    "season": ["2023/24"],
+                    "season_start_date": ["2023-11-01"],
+                    "season_end_date": ["2024-01-31"],
+                }
+            ),
+            [],
+            "season",
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+            ],
+        ),
+        # Observations and ranges are overlapping
+        (
+            pd.DataFrame(
+                data={
+                    "season": ["2023/24"],
+                    "season_start_date": ["2023-11-01"],
+                    "season_end_date": ["2024-01-31"],
+                }
+            ),
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+            ],
+            "season",
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+            ],
+        ),
+        # Observations are contained within ranges
+        (
+            pd.DataFrame(
+                data={
+                    "season": ["2023/24"],
+                    "season_start_date": ["2023-11-01"],
+                    "season_end_date": ["2024-01-31"],
+                }
+            ),
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+                SeasonRange(
+                    season="2024/25", start_date="2024-11-01", end_date="2025-01-31"
+                ),
+            ],
+            "season",
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+                SeasonRange(
+                    season="2024/25", start_date="2024-11-01", end_date="2025-01-31"
+                ),
+            ],
+        ),
+        # Duplicated ranges contained within observations
+        (
+            pd.DataFrame(
+                data={
+                    "season": 10 * ["2023/24"],
+                    "season_start_date": 10 * ["2023-11-01"],
+                    "season_end_date": 10 * ["2024-01-31"],
+                }
+            ),
+            [],
+            "season",
+            [
+                SeasonRange(
+                    season="2023/24", start_date="2023-11-01", end_date="2024-01-31"
+                ),
+            ],
+        ),
+    ],
+)
+def test_exact_outputs_for_select_inputs(
+    observations: pd.DataFrame | None,
+    ranges: list[DateOrSeasonRange],
+    mode: Literal["date", "season"],
+    expected: list[DateOrSeasonRange],
+) -> None:
+    """Test that the exact outputs are returned for select inputs."""
+    output = _infer_ranges_from_observations(observations, ranges, mode)
+    assert isinstance(output, list)
+    cls = DateRange if mode == "date" else SeasonRange
+    assert all(isinstance(r, cls) for r in output)
+    assert set(output) == set(expected)
