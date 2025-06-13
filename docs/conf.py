b9f7cd136f480d448126bb8d6e75a4a400ded970
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-06-13 13:20:05 -0400

    Link documentation pages to GitHub

--- docs/conf.py
+++ docs/conf.py
@@ -7,14 +7,12 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = "vaxflux"
 copyright = "2025, ACCIDDA"
 author = "ACCIDDA"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx.ext.apidoc",
     "sphinx.ext.autodoc",
@@ -24,21 +22,26 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
-
-# -- Options for autodoc -----------------------------------------------------
-autoclass_content = "both"
-autodoc_member_order = "bysource"
-
-# -- Options for pydata theme ------------------------------------------------
+html_theme_options = {
+    "use_edit_page_button": True,
+}
+html_context = {
+    "github_user": "ACCIDDA",
+    "github_repo": "vaxflux",
+    "github_version": "main",
+    "doc_path": "docs",
+}
 html_sidebars: dict[str, list[str]] = {
     "installation": [],
     "introduction": [],
     "getting-started": [],
     "model-details": [],
 }
+
+# -- Options for autodoc -----------------------------------------------------
+autoclass_content = "both"
+autodoc_member_order = "bysource"
