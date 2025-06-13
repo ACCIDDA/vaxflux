1200bde80c15f26da1b3d4ab51e48c649f20d10d
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-06-13 08:49:26 -0400

    Remove packaged dataset
    Including a packaged dataset is not needed since there exists
    `vaxflux.data.get_ncird_weekly_cumulative_vaccination_coverage`. Removed
    corresponding reader utility `vaxflux.data.read_flu_vacc_data`.

--- src/vaxflux/data.py
+++ src/vaxflux/data.py
@@ -2,9 +2,7 @@
 Example datasets and data getting utilities.
 
 This module contains functions for either reading in sample datasets or pulling
-data from external data providers. Current exported functionality includes:
-- `read_flu_vacc_data`
-- `get_ncird_weekly_cumulative_vaccination_coverage`
+data from external data providers.
 """
 
 __all__ = (
@@ -12,7 +10,6 @@ __all__ = (
     "create_logistic_sample_dataset",
     "format_incidence_dataframe",
     "get_ncird_weekly_cumulative_vaccination_coverage",
-    "read_flu_vacc_data",
     "sample_dataset",
 )
 
@@ -20,7 +17,6 @@ __all__ = (
 import io
 import time
 from datetime import datetime
-from importlib import resources
 from typing import Literal, TypedDict, cast
 
 import numpy as np
@@ -29,49 +25,9 @@ import pandas as pd
 import requests
 from scipy.special import expit
 
-from vaxflux import sample_data
 from vaxflux.covariates import CovariateCategories, _covariate_categories_product
 from vaxflux.curves import Curve
 from vaxflux.dates import DateRange, SeasonRange
-
-
-def read_flu_vacc_data() -> npt.NDArray[np.float64]:
-    """
-    Read a sample dataset of flu vaccination data from 2018 to 2024.
-
-    Returns:
-        A structured arrays with five fields named: "season", "end_date", "week",
-        "sort_order", and "doses".
-
-    Examples:
-        >>> from vaxflux.data import read_flu_vacc_data
-        >>> data = read_flu_vacc_data()
-        >>> data[:5]
-        array([(b'2018-2019', b'8/4/18', 30, 1,  0.52),
-               (b'2018-2019', b'8/11/18', 31, 2,  3.23),
-               (b'2018-2019', b'8/18/18', 32, 3, 10.18),
-               (b'2018-2019', b'8/25/18', 33, 4, 19.99),
-               (b'2018-2019', b'9/1/18', 34, 5, 37.38)],
-              dtype=[('season', 'S9'), ('end_date', 'S8'), ('week', '<u8'), ('sort_order', '<u8'), ('doses', '<f8')])
-        >>> data["doses"][:5]
-        array([ 0.52,  3.23, 10.18, 19.99, 37.38])
-
-    """  # noqa: E501
-    csv_file = resources.files(sample_data) / "Flu_Vacc_Data.csv"
-    with csv_file.open() as f:
-        data = np.genfromtxt(
-            f,
-            delimiter=",",
-            dtype=[
-                ("season", "S9"),
-                ("end_date", "S8"),
-                ("week", "u8"),
-                ("sort_order", "u8"),
-                ("doses", "f8"),
-            ],
-            skip_header=1,
-        )
-    return data
 
 
 def get_ncird_weekly_cumulative_vaccination_coverage() -> pd.DataFrame:
